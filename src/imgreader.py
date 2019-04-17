916e78286c81f5136b162d5a4e8ef5b1f1a0aaa4
Srihari Subramanian <sri.sub000@gmail.com>
2019-04-17 05:25:49 -0400

    image train files and labels (but still with ellipses - figuring that out)

--- src/imgreader.py
+++ src/imgreader.py
@@ -1,31 +1,48 @@
 import numpy as np
 import os
 import cv2
+import csv
+from pathlib import Path
 
 
-def getTrainFiles():
+def get_train_files():
     """
     Method returns a 2D numpy array containing the train files and labels
     """
+
+    if Path('D:\\Python Projects\\DistractedDriving\\imagelist.csv').is_file():
+        return
+
     dir = 'D:\\Pictures\\Distracted Driving\\'
     imgdir = dir + 'imgs\\train'
-    x_train = np.empty(dtype=None, shape=0)
-    y_train = np.empty(dtype=None, shape=0)
+    # x_train = np.empty(dtype=None, shape=0)
+    # y_train = np.empty(dtype=None, shape=0)
     i = 0
     imagelist = np.genfromtxt(dir + 'driver_imgs_list.csv', delimiter=',', dtype='U')
-    for distractionfolder in os.listdir(imgdir):
-        for image in os.listdir(imgdir + '\\' + distractionfolder):
-            img = cv2.imread(imgdir + '\\' + distractionfolder + '\\' + image)
-            img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-            x_train = np.append(x_train, img)
-            (labelx, labely) = np.where(imagelist == image)
-            labelx = labelx[0]
-            labely = 1
-            y_train = np.append(y_train, imagelist[labelx][labely])
-            i += 1
-            print(i)
-    return x_train, y_train
+    with open('D:\\Python Projects\\DistractedDriving\\imagelist.csv', 'w') as csvfile:
+        filewriter = csv.writer(csvfile, delimiter=',', quotechar='|')
+        for distractionfolder in os.listdir(imgdir):
+            for image in os.listdir(imgdir + '\\' + distractionfolder):
+                img = cv2.imread(imgdir + '\\' + distractionfolder + '\\' + image)
+                img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+                #x_train = np.append(x_train, img)
+                (labelx, labely) = np.where(imagelist == image)
+                labelx = labelx[0]
+                labely = 1
+                #y_train = np.append(y_train, imagelist[labelx][labely])
+                filewriter.writerow([img, imagelist[labelx][labely]])
+                i += 1
+                print(i)
+    # return x_train, y_train
+
+
+def testlist():
+    if Path('D:\\Python Projects\\DistractedDriving\\imagelist.csv').is_file() == True:
+        with open('D:\\Python Projects\\DistractedDriving\\imagelist.csv', 'r') as csvfile:
+            filereader = csv.reader(csvfile, delimiter=',')
+            for row in filereader:
+                print(row[1])
 
 
 if __name__ == "__main__":
-    print(getTrainFiles())
+    get_train_files()
